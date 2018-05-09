fc387f27e1c435be7ad630bb68e808f72dd9e249
Ted Lorenzen <lorenze3@gmail.com>
2018-05-09 15:42:09 -0500

    Adding Logout, sign in, session validation, creating the global error template, finishing user authorization.  Works locally

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, redirect, session
 from werkzeug import generate_password_hash, check_password_hash
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = 'I am the very model of a modern major general'
     
 @app.route("/")
 def main():
     #return "Yes This App Is Working"
 	 return render_template('index.html')
 
-@app.route('/showSignUp')
-def showSignUp():
+@app.route('/showSignup')
+def showSignup():
     return render_template('signup.html')
 
 @app.route('/signUp',methods=['POST','GET'])
@@ -32,9 +33,11 @@ def signUp():
                msg=reg.fetchall()
             if not('msg' in locals()):
                 conn.commit()
-                return json.dumps({'message':'User created successfully !'})
+                return redirect('/showSignin')
+                #return json.dumps({'message':'User created successfully !'})
             else:
-                return json.dumps({'error':str(msg[0])})
+                 return render_template('error.html',error = 'Username already exists; Sign in or create new account.')
+                 #return json.dumps({'error':str(msg[0])})
         else:
             return json.dumps({'html':'<span>Enter the required fields</span>'})
     except Exception as e:
@@ -42,10 +45,52 @@ def signUp():
     finally:
         cursor.close() 
         conn.close()
+        
+@app.route('/showSignin')
+def showSignin():
+    return render_template('signin.html')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        conn = mysql.connector.connect(user='root', password='Pi3141592',
+                                  host='127.0.0.1',
+                                  database='BucketList')
+        cursor = conn.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        for reg in cursor.stored_results():
+            data=reg.fetchall()
+        if len(data) > 0:
+            if check_password_hash(str(data[0][2]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html','Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Email address not found.')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close
+        conn.close
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error ='Please Sign In')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
         
  
     
