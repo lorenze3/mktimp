6c93e6b153d1663634d10d32d28dce2ae8dc7703
Ted Lorenzen <lorenze3@gmail.com>
2018-05-10 23:46:20 -0500

    cannot get this to output to anywhere but console for uploaded file

--- main.py
+++ main.py
@@ -95,10 +95,10 @@ def logout():
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        extension = os.path.splitext(file.filename)[1]
-        f_name = str(uuid.uuid4()) + extension
+        #extension = os.path.splitext(file.filename)[1]
+        f_name = file.filename#str(uuid.uuid4()) + extension
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
-        return json.dumps({'filename':f_name})
+        return render_template('error.html',error='it worked')#json.dumps({'filename':f_name})
 
 if __name__ == "__main__":
     app.run(debug=True)
