9cda448e1f731f30ec941de41f57fead36c4e6a5
lorenze3 <lorenze3@users.noreply.github.com>
2022-04-30 12:11:05 -0500

    trying a better fix for port

--- main.py
+++ main.py
@@ -32,6 +32,16 @@ m.message=msg1
 app = Flask(__name__)
 app.secret_key = 'I am the very model of a modern major general'
 app.config['UPLOAD_FOLDER'] = 'static/Uploads' 
+#port for mysql in D:\home\data\mysql\MYSQLCONNSTR_localdb.txt
+text_file = open("D:/home/data/mysql/MYSQLCONNSTR_localdb.txt", "r")
+#read whole file to a string
+longstring = text_file.read()
+#close file
+text_file.close()
+#port is after the first colon and is 5 characters long
+mysqlport=int(longstring[longstring.find(':')+1:longstring.find(':')+6])
+uid=longstring[longstring.find("Id=")+3:longstring.find(";",longstring.find("Id=")+1)] 
+pwd=longstring[longstring.find("Password=")+9:] 
 
 @app.route("/")
 def main():
@@ -53,7 +63,7 @@ def signUp():
             if _name and _email and _password:
                 # All Good, let's call MySQL
                 conn = mysql.connector.connect(user='azure', password='6#vWHD_$',
-                                  host='127.0.0.1',port=55008,
+                                  host='127.0.0.1',port=mysqlport,
                                   database='BucketList',autocommit=True)
                 cursor = conn.cursor()
                 _hashed_password = generate_password_hash(_password)
@@ -101,7 +111,7 @@ def validateLogin():
         _username = request.form['inputEmail']
         _password = request.form['inputPassword']
         conn = mysql.connector.connect(user='azure', password='6#vWHD_$',
-                              host='127.0.0.1',port=55008,
+                              host='127.0.0.1',port=mysqlport,
                                   database='BucketList')
         cursor = conn.cursor()
         cursor.callproc('sp_validateLogin',(_username,))
@@ -130,7 +140,7 @@ def userHome():
         if session.get('user'):
             try:
                 conn = mysql.connector.connect(user='azure', password='6#vWHD_$',
-                              host='127.0.0.1',port=55008,
+                              host='127.0.0.1',port=mysqlport,
                                       database='BucketList')
                 cursor = conn.cursor()
                 querystring="Select data_filename from tbl_datafiles where user_id="+str(session.get('user'))+"&& data_resultsname IS NOT NULL;"
@@ -160,7 +170,7 @@ def userHome():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
         try:
             conn = mysql.connector.connect(user='azure', password='6#vWHD_$',
-                              host='127.0.0.1',port=55008,
+                              host='127.0.0.1',port=mysqlport,
                                   database='BucketList',autocommit=True)
             cursor = conn.cursor()
             cursor.callproc('sp_addinputD',(f_name,struid))
@@ -212,7 +222,7 @@ def logout():
         struid=session.get('user')
         try:
             conn = mysql.connector.connect(user='azure', password='6#vWHD_$',
-                                              host='127.0.0.1',port=55008,
+                                              host='127.0.0.1',port=mysqlport,
                                               database='BucketList',autocommit=True)
             cursor = conn.cursor()
             querystring2='update tbl_user set user_lastlogin = NOW() where user_id='+str(struid)+';'
